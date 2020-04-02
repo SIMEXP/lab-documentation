cb34226eecb97bb2763636088b7d121157cb967d
ltetrel <loic.tetrel.pro@gmail.com>
2020-04-02 14:19:59 -0400

    reworked hpc tutorial for simexp doc

---
+++ source/files/par_job.py
@@ -0,0 +1,6 @@
+import sys
+import time
+
+for i in range(int(sys.argv[1]), int(sys.argv[2])):
+    print(i)
+    time.sleep(10)
---
+++ source/files/simple_job.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#SBATCH --time=00:01:00
+#SBATCH --account=def-pbellec
+echo 'Hello HPC world !'
+sleep 5s
